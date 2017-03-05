cfe4f15f63caf38b62f27d534dc138ecaea1ece1
FedorKorolev <korolev.f@icloud.com>
2017-03-05 19:04:46 +0300

    Added Size Interaction

--- SystemFonts/TestDriveViewController.swift
+++ SystemFonts/TestDriveViewController.swift
@@ -10,20 +10,46 @@ import UIKit
 
 class TestDriveViewController: UIViewController {
     
-    @IBOutlet weak var testDriveLabel: UILabel!
-
     var fontName = String()
+    var fontSize = 14 {
+        didSet {
+            if fontSize > 30 {
+                fontSize = 30
+            }
+            testDriveLabel.font = UIFont(name: fontName, size: CGFloat(fontSize))
+            sizeTextField.text? = String(fontSize)
+            stepper.value = Double(fontSize)
+        }
+    }
     
+    
+    @IBOutlet weak var testDriveLabel: UILabel!
+    
+    
+    @IBOutlet weak var sizeTextField: UITextField!
+    
+    @IBAction private func sizeTextFieldChanged(_ sender: UITextField) {
+        if let sizeText = sender.text,
+           let sizeValue = Int(sizeText) {
+            fontSize = sizeValue
+        }
+    }
+    
+    @IBOutlet weak var stepper: UIStepper!
+    
+    @IBAction private func stepperValueChanged(_ sender: UIStepper) {
+        fontSize = Int(sender.value)
+        
+    }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setup()
     }
 
     private func setup() {
         self.title = fontName
         testDriveLabel.text = "Съешь ещё этих мягких французских булчек, да выпей же чаю"
-        testDriveLabel.font = UIFont(name: fontName, size: 14)
     }
     
 
